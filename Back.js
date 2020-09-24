a853140c544794730bac8e5f67f2344bc0602ce8
apandey208 <anishbab95@gmail.com>
2020-09-24 17:51:57 +0530

    changed in js for getting color value

--- Back.js
+++ Back.js
@@ -1,7 +1,8 @@
 var body = document.querySelector("body");
-var input1 = document.querySelector(".give1");
-var input2 = document.querySelector(".give2");
+var input1 = document.getElementById("give1");
+var input2 = document.getElementById("give2");
 var backWhole = document.getElementById("whole");
+
 
 //creating function to adjust the Background picker
 function adjustback(){
@@ -9,12 +10,21 @@ function adjustback(){
     CSS.textContent = backWhole.style.background + ";";
 }
 
-//Adding css mark for getting the ouput color formation
-
-
 //adding event for input1 to change the background 
 input1.addEventListener("input", adjustback);
 
 //adding event to input2 for changing the background 
 input2.addEventListener("input", adjustback);
+
+//Adding css mark for getting the ouput color formation
+function printValue1(){
+	var main = input1.value;
+	document.getElementById("valuePrint1").innerHTML = "The value is :" + main;
+}
+
+function printValue2(){
+	var button = input2.value;
+	document.getElementById("check").innerHTML = "The value is :" + button;
+	
+}
 
